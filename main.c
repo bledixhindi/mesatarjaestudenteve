2b75367451ff6f72f9871e935f238cb20662c825
bledixhindi <54402850+bledixhindi@users.noreply.github.com>
2022-03-25 12:17:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
-int main(void){
 
-int n,i,shuma=0;
-float mesatarja;
-
-printf("Vendos n:");
-scanf("%d",&n);
-
-int notat[n];
-
-printf("Shkruani nr e studenteve:");
-
-for(i=0;i<n;i++)
+int main()
 {
-    scanf("%d",&notat[i]);
-    shuma += notat[i];
-}
-mesatarja =(float)shuma/n;
+    int i,j,rows,colon,v[10][10];
+	float Sum;
+	int c;
+	int max;
+	int min;
 
-printf("Mesatarja eshte %0.2f\n",mesatarja);
+    printf("Vendos i,j:");
+    scanf("%d %d",&i,&j);
 
-printf("Notat kaluese:");
-for(i=0;i<n;i++)
-{
-    if(notat[i]>4)
+        printf("Vendos vendos numrat: \n");
+    for(rows=0;rows<i;rows++)
     {
-        printf("\t%d",notat[i]);
+               for(colon=0;colon<j;colon++)
+               {
+                   printf("Vendos vleren ne matricat v[%d][%d]:",rows,colon);
+                   scanf("%d",&v[rows][colon]);
+               }
     }
-    
-    
-}
+
+     printf("Matrica eshte:\n");
+    for(rows = 0; rows < i; rows++)
+    {
+            for(colon=0;colon<j;colon++)
+        {
+            printf("%d\t",v[rows][colon]);
+        }
+        printf("\n");
+    }
+
+    for(rows = 0; rows < i; rows++)
+    {
+            Sum=0;
+            for(colon=0;colon<j;colon++)
+            {
+                Sum=Sum + v[rows][colon];
+            }
+        printf("Mesatrja e cdo sudenti eshte :  %0.2f \n", Sum/3 );
+
+    }
+
+    for(rows = 0; rows < j; rows++)
+    {
+            c=0;
+            for(colon=0;colon<i;colon++)
+            {
+                c=c + v[colon][rows];
+                if(v[colon][rows]>max)
+				{
+					max=v[colon][rows];
+
+				}
+				if(v[colon][rows]<min)
+
+				{
+					min=v[colon][rows];
+				}
+            }
+        printf("Nota per cdo kuic :  %0.2f \n", (float)c/5);
+
+    }
+             printf("Max eshte :  %d \n", max);
+             printf("Min eshte :  %d \n", min);
+
+     return 0;
 
 }
